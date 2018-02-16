84323222f1a0987a9096fb0e218b9ec72e9cf55b
Jake <jakewgoodman@gmail.com>
2018-02-16 09:47:24 -0500

    Added all card values in Value.java

--- src/deckofcards/Card.java
+++ src/deckofcards/Card.java
@@ -3,6 +3,11 @@ package deckofcards;
 public class Card {
     private Suit suit;
     private Value value;
+
+    public Card (Suit suit, Value value) {
+        this.suit = suit;
+        this.value = value;
+    }
 }
 
 
