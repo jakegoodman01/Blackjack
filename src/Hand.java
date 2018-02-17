848016be3189f886ccfdaa15aeb27235b5146d34
Jake <jakewgoodman@gmail.com>
2018-02-16 21:35:18 -0500

    Added method hit() and getHandValue() to Hand.java

--- src/Hand.java
+++ src/Hand.java
@@ -1,7 +1,33 @@
 import deckofcards.Card;
+import deckofcards.Deck;
 
 import java.util.ArrayList;
 
 public class Hand extends ArrayList<Card> {
-    
+
+    private Deck deck;
+
+    public Hand (Deck deck) {
+        this.deck = deck;
+    }
+
+    public boolean hit() {
+        Card card;
+        add(card = deck.deal());
+        System.out.printf("You got: %s%n", card);
+        return getHandValue() <= 21;
+    }
+
+
+    /**
+     * Finds the value of the given hand
+     * @return The int value of the given hand
+     */
+    public int getHandValue() {
+        int handValue = 0;
+        for (Card card : this) {
+            handValue += card.getCardValue();
+        }
+        return handValue;
+    }
 }
