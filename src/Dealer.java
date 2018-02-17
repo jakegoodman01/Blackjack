acb97d8aa3350284591b2143d5b6be38b70463f6
Jake <jakewgoodman@gmail.com>
2018-02-16 20:48:56 -0500

    Added methods endGame() and getHandValue() to Dealer.java

--- src/Dealer.java
+++ src/Dealer.java
@@ -30,11 +30,43 @@ public class Dealer {
         playerCards.add(deck.deal());
     }
 
+    /**
+     * Creates a new deck and then shuffles it, and clears the player and dealer cards
+     */
+    public void endGame() {
+        deck = new Deck();
+        deck.shuffle();
+        dealerCards.clear();
+        playerCards.clear();
+    }
+
+    /**
+     * Getter for the dealer's hand
+     * @return The dealer's current cards
+     */
     public List<Card> getDealerCards() {
         return dealerCards;
     }
 
+    /**
+     * Getter for the player's cards
+     * @return The player's current cards
+     */
     public List<Card> getPlayerCards() {
         return playerCards;
+    }
+
+
+    /**
+     * Finds the value of the given hand
+     * @param hand A list of cards in the current hand
+     * @return The int value of the given hand
+     */
+    private int getHandValue(List<Card> hand) {
+        int handValue = 0;
+        for (Card card : hand) {
+            handValue += card.getCardValue();
+        }
+        return handValue;
     }
 }
